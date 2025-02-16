2abc78fa1aae30ba84efe83f5be9f8a03c3bf181
Tamar khuskivadze (თამარ ხუსკივაძე) <thamuna.khuskivadze@gmail.com>
2025-02-16 13:21:48 +0400

    add corect logic

--- sale.js
+++ sale.js
@@ -4,8 +4,6 @@
 const adminSale = 20; // რამდენ პროცენტიანი ფასდაკლება ეკუთვნის ადმინისტრატორს
 const frequentClientSale = 15; // რამდენ პროცენტიანი ფასდაკლება ეკუთვნის ვებსაიტის ხშირ კლიენტს
 const studentSale = 10; // რამდენ პროცენტიანი ფასდაკლება ეკუთვნის სტუდენტს
- 
-
 // მომხმარებელს სურს პროდუქციის შეძენა, ამ პროცესის ასასახად გვაქვს შემდეგი ცვლადები:
 
 const userBalance = 10;
@@ -14,22 +12,22 @@ const cartTotal = 15;
 const userIsAdmin = true;   
 const userIsFrequentClient = false;  
 const userIsStudent = false;  
-
-let discauntAccount;
-let finalPrice  ;
 let paymentComplete ;
+
+let discauntAccount;  // გამოვთვალო რამდენია ფასდაკლება 
+let finalPrice ;  // გავიგო რა იქნება საბოლო ფასი 
  
-
-if (userIsAdmin) {
-    discauntAccount = cartTotal * (adminSale / 100);
-} else if (userIsFrequentClient) {
-    discauntAccount = cartTotal * (frequentClientSale / 100);
-} else if (userIsStudent) {
-    discauntAccount = cartTotal * (studentSale / 100);
-}
-finalPrice = cartTotal - discauntAccount;
-paymentComplete = userBalance >= finalPrice;
-
+ 
+// if (userIsAdmin) {
+//     discauntAccount = cartTotal * (adminSale / 100);
+// } else if (userIsFrequentClient) {
+//     discauntAccount = cartTotal * (frequentClientSale / 100);
+// } else if (userIsStudent) {
+//     discauntAccount = cartTotal * (studentSale / 100);
+// }
+// finalPrice = cartTotal - discauntAccount;
+// paymentComplete = userBalance >= finalPrice;
+// console.log(paymentComplete);
 
 
 // გამოთვალეთ წარმატებით შეძლო თუ არა პროდუქტის შეძენა მომხმარებელმა.
@@ -48,3 +46,17 @@ paymentComplete = userBalance >= finalPrice;
 
  
  
+let sale = 0
+
+if (userIsAdmin) {
+    sale = adminSale
+}else if (userIsFrequentClient) {
+    sale = frequentClientSale
+}else if (userIsStudent) {
+    sale = studentSale
+}
+let discounted = cartTotal / 100 * (100 - sale)
+
+paymentComplete = userBalance >= discounted
+
+console.log(paymentComplete)
